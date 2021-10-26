bcd5b32eb0b46d5371ae77486a5e257520c3e1a7
rohithchinta <rohithchintamaneni@hotmail.com>
2021-10-26 10:53:01 -0700

    adding linkedlist cycle detention

---
+++ DataStructures/DataStructures/LinkedList/Medium/LinkedListCycle2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DataStructures.LinkedList.Medium
+{
+    class LinkedListCycle2
+    {
+        public ListNode DetectCycle(ListNode head)
+        {
+            var slow = head;
+            var fast = head;
+            var s2 = head;
+            bool iscycle = false;
+
+            while (fast != null && fast.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+                if (slow == fast)
+                {
+                    iscycle = true;
+                    break;
+                }
+
+            }
+
+            while (iscycle)
+            {
+                if (s2 == slow) return s2;
+                s2 = s2.next;
+                slow = slow.next;
+            }
+
+
+            return null;
+        }
+    }
+}
