e4a2105c2c758e83b3734dbc342fe3ef205ae350
rohithchinta <rohithchintamaneni@hotmail.com>
2021-10-26 22:08:44 -0700

    adding code for linkedlist

---
+++ DataStructures/DataStructures/LinkedList/Hard/MergeKLinkedListProblem.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.LinkedList.Hard
+{
+    class MergeKLinkedListProblem
+    {
+        public ListNode MergeKLists(ListNode[] lists)
+        {
+            if (lists.Length == 0) return null;
+            if (lists.Length == 1) return lists[0];
+
+            var head = MergeTwolinkedLists(lists[0], lists[1]);
+            for (int i = 2; i < lists.Length; i++)
+            {
+                head = MergeTwolinkedLists(head, lists[i]);
+            }
+
+
+            return head;
+
+        }
+
+        private ListNode MergeTwolinkedLists(ListNode l1, ListNode l2)
+        {
+            //if (h1 == null) return h2;
+
+            //if (h2 == null) return h1;
+            //ListNode dummyNode = new ListNode(0);
+            //var head = dummyNode;
+
+            //while (h1 != null || h2 != null)
+            //{
+            //    if (h2 == null|| ( h1 != null && h1.val < h2.val))
+            //    {
+            //        dummyNode.next = h1;
+            //        h1 = h1.next;
+
+            //    }
+            //    else
+            //    {
+            //        dummyNode = h2;
+            //        h2 = h2.next;
+            //    }
+
+            //    dummyNode = dummyNode.next;
+            //}
+
+            //return head.next;
+
+
+            if (l1 == null) return l2;
+
+            if (l2 == null) return l1;
+
+
+            ListNode DummyNode = new ListNode(0);
+            var head = DummyNode;
+
+            while (l1 != null || l2 != null)
+            {
+                if (l2 == null || (l1 != null && l1.val <= l2.val))
+                {
+                    DummyNode.next = l1;
+                    l1 = l1.next;
+                }
+                else
+                {
+                    DummyNode.next = l2;
+                    l2 = l2.next;
+                }
+
+                DummyNode = DummyNode.next;
+            }
+
+
+
+
+            return head.next;
+        }
+    }
+}
--- DataStructures/DataStructures/LinkedList/MergeTwoSortedLists.cs
+++ DataStructures/DataStructures/LinkedList/MergeTwoSortedLists.cs
@@ -40,5 +40,38 @@ namespace DataStructures.LinkedList
             return head.next;
         }
 
+        // If conditions are simple
+        public ListNode MergeTwoLists_LittleImproved(ListNode l1, ListNode l2)
+        {
+
+            ListNode h = new ListNode(0);
+            ListNode ans = h;
+
+            while (l1 != null && l2 != null)
+            {
+                if (l1.val < l2.val)
+                {
+                    h.next = l1;
+                    l1 = l1.next;
+                }
+                else
+                {
+                    h.next = l2;
+                    l2 = l2.next;
+                }
+                h = h.next;
+            }
+
+            if (l1 == null)
+            {
+                h.next = l2;
+            }
+            if (l2 == null)
+            {
+                h.next = l1;
+            }
+            return ans.next;
+        }
+
     }
 }
