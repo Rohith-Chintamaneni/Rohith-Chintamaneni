1108c01149c31c8cd51e77538ce8c33737f4d480
rohithchinta <rohithchintamaneni@hotmail.com>
2022-02-23 19:34:09 -0700

    adding linkedlist cycle to List

---
+++ DataStructures/DataStructures/LinkedList/LinkedListCycle141.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.LinkedList
+{
+    class LinkedListCycle141
+    {
+        public bool HasCycle(ListNode head)
+        {
+            if (head == null || head.next == null || head.next.next == null)
+                return false;
+
+            var dummynode = head;
+            var slow = head;
+            var fast = head.next.next;
+            bool flag = false;
+            while (fast != null && fast.next != null)
+            {
+                if (slow == fast)
+                {
+                    flag = true;
+                    return flag;
+                }
+
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+
+            return flag;
+
+        }
+    }
+}
