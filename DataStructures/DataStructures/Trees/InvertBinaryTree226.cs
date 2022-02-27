260d54996be1837af98ba5e81c490d0726a76500
rohithchinta <rohithchintamaneni@hotmail.com>
2022-02-27 00:25:18 -0700

    adding Invertt Binary tree to solution

---
+++ DataStructures/DataStructures/Trees/InvertBinaryTree226.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Trees
+{
+    class InvertBinaryTree226
+    {
+        public TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null) return root;
+            TreeNode left = InvertTree(root.left);
+            TreeNode right = InvertTree(root.right);
+
+            root.left = right;
+            root.right = left;
+            return root;
+        }
+    }
+}
