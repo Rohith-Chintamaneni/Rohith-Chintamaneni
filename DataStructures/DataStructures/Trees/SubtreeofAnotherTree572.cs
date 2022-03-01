874bdbdc3454dc0b16af4e0e341c2d28508c3c76
rohithchinta <rohithchintamaneni@hotmail.com>
2022-02-28 19:36:19 -0700

    adding problem for SubtreeOfAnotherTree

---
+++ DataStructures/DataStructures/Trees/SubtreeofAnotherTree572.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Trees
+{
+    class SubtreeofAnotherTree572
+    {
+        public bool IsSubtree(TreeNode root, TreeNode subRoot)
+        {
+            if (root == null) return false;
+
+            if (IsSame(root, subRoot)) return true;
+
+            return IsSubtree(root.left, subRoot) || IsSubtree(root.right, subRoot);
+
+        }
+
+        protected bool IsSame(TreeNode t, TreeNode s)
+        {
+            if (t == null && s == null) return true;
+            if (t == null || s == null) return false;
+            if (t.val != s.val) return false;
+
+            return IsSame(t.left, s.left) && IsSame(t.right, s.right);
+        }
+    }
+}
