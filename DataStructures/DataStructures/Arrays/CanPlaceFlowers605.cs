df9385170a26c6f2d27e7cc1c8ddc25b70a663a4
rohithchinta <rohithchintamaneni@hotmail.com>
2022-03-04 10:14:49 -0700

    adding a method for FlowerBed

---
+++ DataStructures/DataStructures/Arrays/CanPlaceFlowers605.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Arrays
+{
+    class CanPlaceFlowers605
+    {
+        public bool CanPlaceFlowers(int[] flowerbed, int n)
+        {
+
+            int j = 0, len = flowerbed.Length - 1;
+            int count = 0;
+            for (int i = 0; i < flowerbed.Length; i++)
+            {
+                if (flowerbed[i] == 0)
+                {
+                    if ((i == j || flowerbed[i - 1] == 0) && (i == len || flowerbed[i + 1] == 0))
+                    {
+                        flowerbed[i] = 1;
+                        count++;
+                    }
+                }
+            }
+            //Console.WriteLine(count);
+            return count >= n;
+        }
+    }
+}
